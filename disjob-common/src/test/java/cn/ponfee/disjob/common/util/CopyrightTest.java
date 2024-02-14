6b3fcef3f4c3b10166db3edec8ea0ecf462bf75e
Ponfee <ponfee.cn@gmail.com>
2024-02-14 11:08:24 +0800

    modify copyright

--- disjob-common/src/test/java/cn/ponfee/disjob/common/util/CopyrightTest.java
+++ disjob-common/src/test/java/cn/ponfee/disjob/common/util/CopyrightTest.java
@@ -1,10 +1,18 @@
-/* __________              _____                                                *\
-** \______   \____   _____/ ____\____   ____    Copyright (c) 2017-2023 Ponfee  **
-**  |     ___/  _ \ /    \   __\/ __ \_/ __ \   http://www.ponfee.cn            **
-**  |    |  (  <_> )   |  \  | \  ___/\  ___/   Apache License Version 2.0      **
-**  |____|   \____/|___|  /__|  \___  >\___  >  http://www.apache.org/licenses/ **
-**                      \/          \/     \/                                   **
-\*                                                                              */
+/*
+ * Copyright 2022-2024 Ponfee (http://www.ponfee.cn/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package cn.ponfee.disjob.common.util;
 
@@ -32,10 +40,10 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 @Disabled
 public class CopyrightTest {
 
-    private static final String OLD_COPYRIGHT_KEYWORD = " Copyright (c) 2017-2023 Ponfee ";
-    private static final String NEW_COPYRIGHT_KEYWORD = " Copyright (c) 2017-2024 Ponfee ";
+    private static final String OLD_COPYRIGHT_KEYWORD = "\n * Copyright 2022-2023 Ponfee (http://www.ponfee.cn/)\n";
+    private static final String NEW_COPYRIGHT_KEYWORD = "\n * Copyright 2022-2024 Ponfee (http://www.ponfee.cn/)\n";
 
-    private static final String BASE_DIR = MavenProjects.getProjectBaseDir();
+    private static final String BASE_DIR = MavenProjects.getProjectBaseDir(); // + "/src/test/java/cn/ponfee/disjob/common/spring/a";
     private static final String COPYRIGHT = ThrowingSupplier.doChecked(
         () -> IOUtils.resourceToString("copy-right.txt", UTF_8, CopyrightTest.class.getClassLoader())
     );
@@ -48,16 +56,16 @@ public class CopyrightTest {
                 return;
             }
             try {
-                if (text.contains("** \\______   \\____   _____/ ____\\____   ____   " + OLD_COPYRIGHT_KEYWORD + " **")) {
+                if (text.contains(OLD_COPYRIGHT_KEYWORD)) {
                     Writer writer = new FileWriter(file.getAbsolutePath());
                     IOUtils.write(COPYRIGHT, writer);
-                    IOUtils.write(text.substring(83 * 7 + 1), writer);
+                    IOUtils.write(text.substring(621), writer);
                     writer.flush();
                     writer.close();
                     return;
                 }
 
-                if (!text.contains("** \\______   \\____   _____/ ____\\____   ____   " + NEW_COPYRIGHT_KEYWORD)) {
+                if (!text.contains(NEW_COPYRIGHT_KEYWORD)) {
                     Writer writer = new FileWriter(file.getAbsolutePath());
                     IOUtils.write(COPYRIGHT, writer);
                     IOUtils.write(text, writer);
