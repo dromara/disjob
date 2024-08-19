7e4a07dba8a0a68a2fe8a2feee3bfe48095e37c9
Ponfee <ponfee.cn@gmail.com>
2024-08-19 21:25:34 +0800

    2.1.4-SNAPSHOT

--- disjob-core/src/main/java/cn/ponfee/disjob/core/base/JobConstants.java
+++ disjob-core/src/main/java/cn/ponfee/disjob/core/base/JobConstants.java
@@ -28,7 +28,7 @@ public final class JobConstants {
     /**
      * Version
      */
-    public static final String VERSION = "2.1.3";
+    public static final String VERSION = "2.1.4-SNAPSHOT";
 
     /**
      * Process batch size
