7d2aa08c973795bb518d1ba8e94bf316be690b49
Ponfee <ponfee.cn@gmail.com>
2026-05-23 22:39:40 +0800

    registry database

--- disjob-samples/disjob-samples-springboot-merged/src/main/java/cn/ponfee/disjob/samples/merged/MergedApplication.java
+++ disjob-samples/disjob-samples-springboot-merged/src/main/java/cn/ponfee/disjob/samples/merged/MergedApplication.java
@@ -16,12 +16,18 @@
 
 package cn.ponfee.disjob.samples.merged;
 
+import cn.ponfee.disjob.common.spring.JdbcTemplateWrapper;
 import cn.ponfee.disjob.id.snowflake.db.DbSnowflakeIdGenerator;
+import cn.ponfee.disjob.registry.database.configuration.DatabaseServerRegistryAutoConfiguration;
 import cn.ponfee.disjob.supervisor.configuration.EnableSupervisor;
 import cn.ponfee.disjob.test.executor.SamplesJobExecutorPackage;
 import cn.ponfee.disjob.worker.configuration.EnableWorker;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import static cn.ponfee.disjob.supervisor.dao.SupervisorDataSourceConfig.SPRING_BEAN_NAME_JDBC_TEMPLATE;
 
@@ -45,6 +51,15 @@ public class MergedApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MergedApplication.class, args);
+    }
+
+    /**
+     * 当`supervisor`与`registry database`使用的是同一个数据库时，可以使用以下方式配置`registry database`的数据库
+     */
+    @ConditionalOnMissingBean(name = DatabaseServerRegistryAutoConfiguration.SPRING_BEAN_NAME_JDBC_TEMPLATE_WRAPPER)
+    @Bean(DatabaseServerRegistryAutoConfiguration.SPRING_BEAN_NAME_JDBC_TEMPLATE_WRAPPER)
+    public JdbcTemplateWrapper databaseRegistryJdbcTemplateWrapper(@Qualifier(SPRING_BEAN_NAME_JDBC_TEMPLATE) JdbcTemplate jdbcTemplate) {
+        return JdbcTemplateWrapper.of(jdbcTemplate);
     }
 
 }
